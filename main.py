a4be34973d977f4e42bc57568bd12bc84b2c6b83
wweemsec <elicookweems@gmail.com>
2026-04-15 11:37:34 -0400

    start of airport

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
 print("this is the main")
 print("HI")
+
+for line in f:
+    line = line.strip()
+    fields = line.split(",")
+
+    if len(fields) != 9:
+        invalid_rows = invalid_rows + 1 
+        print("Invalid row: ", line)
+        continue
+    
+    try:
+        year = int(fields[0])
+    except:
+        invalid_rows = invalid_rows + 1 
+        print("Invalid row: ", line)
+        continue
+
+    if year != 2024:
+        invalid_rows = invalid_rows + 1
+        print("Invalid row: ", line)
+        continue
