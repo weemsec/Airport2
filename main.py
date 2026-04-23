62f99afe2c804c325ad1e787bbbac84f9a74397a
wweemsec <elicookweems@gmail.com>
2026-04-23 17:03:35 -0400

    1st part good

--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 #Fermin
         
 #takes value turns to int or no vlaue if blank
-def to_int_or_none ():
-    value = value.strip
+def to_int_or_none(value):
+    value = value.strip()
     if value == "":
         return None
     return int(value)
@@ -16,28 +16,33 @@ errfile = open("errors_log.txt", "w")
 valid_rows = 0
 invalid_rows = 0
 f.readline()
+line_number = 2
+
 for line in f:
     line = line.strip()
     fields = line.split(",")
 
     if len(fields) != 9:
-        invalid_rows = invalid_rows + 1 
-        errfile.write("Invalid row: Wrong nymber of fields\n")
+        invalid_rows += 1 
+        errfile.write(f"Line {line_number}: Wrong nymber of fields\n")
         errfile.write("Raw data: " + line + '\n\n')
+        line_number += 1
         continue
     
     try:
         year = int(fields[0])
     except:
-        invalid_rows = invalid_rows + 1 
-        errfile.write("Invalid row: Not a year number \n")
-        errfile.write("Raw data: " + line + '\n')
+        invalid_rows += 1 
+        errfile.write(f"Line {line_number}: Not a year number \n")
+        errfile.write("Raw data: " + line + '\n\n')
+        line_number += 1
         continue
 
     if year != 2024:
-        invalid_rows = invalid_rows + 1
-        errfile.write("Invalid row: Not in year 2024 \n")
-        errfile.write("Raw data: " + line + '\n')
+        invalid_rows += 1
+        errfile.write(f"Line {line_number}: Not in year 2024 \n")
+        errfile.write("Raw data: " + line + '\n\n')
+        line_number += 1
         continue
     
 
@@ -45,48 +50,59 @@ for line in f:
         month = int(fields[1])
         day = int(fields[2])
     except:
-        invalid_rows = invalid_rows + 1
-        errfile.write("Invalid row: Not a Number \n")
-        errfile.write("Raw data: " + line + '\n')
+        invalid_rows += 1
+        errfile.write(f"Line {line_number}: Not a Number \n")
+        errfile.write("Raw data: " + line + '\n\n')
+        line_number += 1
         continue
     #checks to see if its a real number for the month
     if month < 1 or month > 12:
-        errfile.write("Invalid row: Not a real Month\n")
-        errfile.write("Raw data: " + line + '\n')
+        invalid_rows += 1
+        errfile.write(f"Line {line_number}: Not a real Month\n")
+        errfile.write("Raw data: " + line + '\n\n')
+        line_number += 1
         continue
     #check for the right days in febuary
     if month == 2:
-        if day > 29 or day < 0:
-            errfile.write("Invalid row: Day does not exsit in month \n")
-            errfile.write("Raw data: " + line + '\n')
+        if day < 1 or day > 29:
+            invalid_rows += 1
+            errfile.write(f"Line {line_number}: Day does not exsit in month \n")
+            errfile.write("Raw data: " + line + '\n\n')
+            line_number += 1
             continue
     #Makes sure  the month has right day
     elif month in [4,6,9,11]:
-        if day != 30:
-            errfile.write("Invalid row: Day does not exsit in month\n")
-            errfile.write("Raw data: " + line + '\n')
+        if day < 1 or day > 30:
+            invalid_rows += 1
+            errfile.write(f"Line {line_number}: Day does not exsit in month\n")
+            errfile.write("Raw data: " + line + '\n\n')
+            line_number += 1
             continue
     else:
-        if day != 31:
-            errfile.write("Invalid row: Day does not exsit in month\n")
-            errfile.write("Raw data: " + line + '\n')
+        if day < 1 or day > 31:
+            invalid_rows += 1
+            errfile.write(f"Line {line_number}: Day does not exsit in month\n")
+            errfile.write("Raw data: " + line + '\n\n')
+            line_number += 1
             continue
 
 
     #OP_UNIQUE_CARRIER Validation
     carrier = fields[3].strip()
     if len(carrier) != 2:
-        invalid_rows = invalid_rows + 1
-        errfile.write("Invalid row: Wrong carrier\n")
-        errfile.write("Raw data: " + line + '\n')
+        invalid_rows += 1
+        errfile.write(f"Line {line_number}: Wrong carrier\n")
+        errfile.write("Raw data: " + line + '\n\n')
+        line_number += 1
         continue
 
     # Origin must be CLT or ATL
     origin = fields[4]
     if origin not in ["CLT","ATL"]:
         invalid_rows += 1
-        errfile.write("Invalid row: Origin is not CLT or ATL\n")
-        errfile.write("Raw data: " + line + '\n')
+        errfile.write(f"Line {line_number}: Origin is not CLT or ATL\n")
+        errfile.write("Raw data: " + line + '\n\n')
+        line_number += 1
         continue
 
     #If the code reaches here, the row has passed the first 4 steps!
@@ -95,8 +111,9 @@ for line in f:
     destination = fields[5].strip()
     if len(destination) != 3:
         invalid_rows += 1
-        errfile.write("Invalid row: Worng destination\n")
-        errfile.write("Raw data: " + line + '\n')
+        errfile.write(f"Line {line_number}: Worng destination\n")
+        errfile.write("Raw data: " + line + '\n\n')
+        line_number += 1
         continue
     
     try:
@@ -104,8 +121,9 @@ for line in f:
         arr_delay = to_int_or_none(fields[7])
     except:
         invalid_rows += 1
-        errfile.write("Invalid row: \n")
-        errfile.write("Raw data: " + line + '\n')
+        errfile.write(f"Line {line_number}: Delay value is not a number\n")
+        errfile.write("Raw data: " + line + '\n\n')
+        line_number += 1
         continue
 
     #Cancelled changing from string to int 
@@ -113,26 +131,28 @@ for line in f:
         cancelled = int(fields[8])
     except:
         invalid_rows += 1
-        errfile.write("Invalid row: \n")
-        errfile.write("Raw data: " + line + '\n')
+        errfile.write(f"Line {line_number}: Cancelled is not a number or missing data\n")
+        errfile.write("Raw data: " + line + '\n\n')
+        line_number += 1
         continue
     #checks if 1 or 0 
-    if cancelled > 1 or cancelled < 0:
+    if cancelled not in [0, 1]:
         invalid_rows += 1
-        errfile.write("Invalid row: \n")
-        errfile.write("Raw data: " + line + '\n')
+        errfile.write(f"Line {line_number}: Cancelled is not 0 or 1\n")
+        errfile.write("Raw data: " + line + '\n\n')
+        line_number += 1
         continue
     # cancelled flights may not have delay values
     if cancelled == 1:
         if dep_delay is not None or arr_delay is not None:
             invalid_rows += 1
-            print("Invlaid number")
-    else:
-        if dep_delay is not None and arr_delay is None:
-            invalid_rows += 1
-            print("Invalid number")
+            errfile.write(f"Line {line_number}: Is cancelled but contains a delay vlaue\n")
+            errfile.write("Raw data: " + line + '\n\n')
+            line_number +=1
+            continue
 
     valid_rows += 1
+    line_number += 1
 
 f.close()
 errfile.close()
